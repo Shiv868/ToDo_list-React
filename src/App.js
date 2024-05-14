cf7c28b341a6616d8a7e395a3015fd8c50690804
shiva <shivamohan868@gmail.com>
2024-05-14 22:35:45 +0530

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    function App() {
+        const [todos, setTodos] = useState([])
+        const [inputValue, setInputValue] = useState('')
+      
+      function handleChange(e){
+        setInputValue(e.target.value)
+      }
+      
+      function handleSubmit(e){
+        e.preventDefault()
+        setTodos([...todos, inputValue])
+        setInputValue('')
+      }
+
+      const handleDelete = (index) => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+      };
+            
+      return (
+        <div className="container">
+           <center> <h1 className="heading">Todo List</h1></center>
+            <form className="form" onSubmit={handleSubmit}>
+                <input
+                    className="input"
+                    type='text'
+                    value={inputValue}
+                    onChange={handleChange}
+                    placeholder="Enter a new todo"
+                />
+                <button className="button">Add Todo</button>
+            </form>
+            <ul className="list">
+                {todos.map((todo, index) => (
+                    <li className="item" key={index}>
+                        <span className="text">{todo}</span>
+                        <button className="delete-button" onClick={() => handleDelete(index)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
-
-export default App;
+export default App ;
